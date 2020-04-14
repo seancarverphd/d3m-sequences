bb60355d21589c556babf932c6afc6a8070955f1
Sean Carver <seancarverphd@gmail.com>
2020-04-14 14:31:14 -0400

    Now "import crawl" also counts the number of primitives (with repeats) in the data set.

--- file_crawler/crawl.py
+++ file_crawler/crawl.py
@@ -1,9 +1,12 @@
+import json
 import os
+
 import config
 
 institution_dirs = os.listdir(config.data_home)
 n_submissions = 0
 n_pipelines = 0
+n_primitives = 0
 for institution_dir in institution_dirs:
     submissions = os.listdir(config.data_home + '/' + institution_dir)
     n_submissions += len(submissions)
@@ -16,7 +19,15 @@ for institution_dir in institution_dirs:
                 continue
             else:
                 assert pipeline[-4:] == 'json'
+                with open(config.data_home + '/' + institution_dir + '/' + submission + '/' + often_just_one[0] + '/EVALUATION/pipelines_ranked/' + pipeline) as f:
+                    d = json.load(f)
+                list_of_step_ids = []
+                for step in d['steps']:
+                    list_of_step_ids.append(step['primitive']['id'])
+                new_pipeline = {'pipeline' : d['id'], 'primitives' : list_of_step_ids}
                 n_pipelines += 1
+                n_primitives += len(d['steps'])
 print(len(institution_dirs), "Institutional Directories")
 print(n_submissions, "Submissions") 
 print(n_pipelines, "Pipelines")
+print(n_primitives, "Primitives")
