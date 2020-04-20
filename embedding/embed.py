a41633fd435f2c29a5003d7d4db72802d2b25d5b
Sean Carver <seancarverphd@gmail.com>
2020-04-20 16:09:31 -0400

    Added embed to run the embedding

---
+++ embedding/embed.py
@@ -0,0 +1,17 @@
+import pickle
+from sgt import Sgt
+
+with open("input.pickle", "rb") as f:
+    corpus = pickle.load(f)
+    alphabets = pickle.load(f)
+    pipeline_list = pickle.load(f)
+    code2id = pickle.load(f)
+    id2code = pickle.load(f)
+
+s_g_t = Sgt(kappa=5, lengthsensitive=True)
+
+s = s_g_t.fit_transform(corpus)
+
+with open("output.pickle", "wb") as f:
+    pickle.dump(s, f)
+
