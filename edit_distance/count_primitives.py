bc36f681c50e6bd3a166a3060d9354eceeeb9fcf
Sean Carver <seancarverphd@gmail.com>
2020-04-28 16:16:54 -0400

    Refactored count_primitives

--- edit_distance/count_primitives.py
+++ edit_distance/count_primitives.py
@@ -10,18 +10,25 @@ with open("translators.pickle", "rb") as f:
     c2i = pickle.load(f)
     i2c = pickle.load(f)
 
-unique_problems = set(problems.problem) 
+all_characters_list = [chr(k) for k in range(0x21, 0x7f)]
+all_characters_string = ''.join(all_characters_list)
 
-n = 0
 n_primitives = []
+
+unique_problems = set(problems['problem']) 
 u_problems = list(unique_problems)
 for u_problem in u_problems:
     problems_one_type = problems[problems['problem'] == u_problem]
-    seq_list = [] 
-    for sequence in problems_one_type['sequence']:
-        seq_list.append(sequence)
-    seq = '-'.join(seq_list)
-    n_primitives.append(len(set(seq.split(sep='-'))))
+    pipeline_list = [] 
+    for pipeline_sequence in problems_one_type['sequence']:
+        pipeline_list.append(pipeline_sequence)
+    concat_pipelines = '-'.join(pipeline_list)  # joining all pipelines, repeating primitives
+    set_primitives2_for_problem = set(concat_pipelines.split(sep='-'))  # contains just the unique primitives
+    list_primitives2_for_problem = list(set_primitives2_for_problem)
+    string_primitives2_for_problem = '-'.join(list_primitives2_for_problem)
+    n_primitives.append(len(list_primitives2_for_problem))
 
 print(pd.DataFrame({'problem': u_problems, 'number_primitives': n_primitives}))
 print("Max number of primitives:", max(n_primitives))
+print(len(all_characters_string), "Characters in Alphabet Set:", all_characters_string)
+
