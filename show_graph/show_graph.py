37d8a057c3c788067111b0cadcfcd4a8e6916e34
Sean Carver <seancarverphd@gmail.com>
2020-04-30 15:56:33 -0400

    Finalized show_graph, added seed removed test code and debugging code

--- show_graph/show_graph.py
+++ show_graph/show_graph.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd
 import pickle
+import random
 
 import networkx as nx
 
@@ -10,23 +11,28 @@ with open("single_problem_data.pickle", "rb") as f:
 
 performer_colors = {'cmu':'red', 'isi':'orange', 'mit':'yellow', 'nyu':'green', 'nyu_2':'blue', 'sri':'purple', 'tamu':'grey', 'ucb':'brown', 'uncharted':'pink'}
 
-def compute_one(i):
+def compute_one(i, seed=None, performer2watch=None):
     G = nx.Graph()
+    maxd = 0
     node_labels = single_problems[i]['pipeline_num']
     n_nodes = len(node_labels)
     G.add_nodes_from(node_labels)
     cols = [performer_colors[k] for k in single_problems[i]['performers']]
-    for a in range(n_nodes-1):
-        for b in range(a+1, n_nodes):
+    for a in range(n_nodes-1):  # looping over all unique pairs of nodes
+        for b in range(a+1, n_nodes):  # looping over all unique pairs of nodes
             dist = single_problems[i]['distances'][a,b]
+            if single_problems[i]['performers'][a] == performer2watch and single_problems[i]['performers'][b] == performer2watch:  # selects performer2watch for pairwise comparisons
+                print(dist, single_problems[i]['pipeline_2'][a], single_problems[i]['pipeline_2'][b])  # prints distance, and both sequences
+            if maxd < dist:  # to figure out maximum distance for problem
+                maxd = dist
             similarity = 1000. if dist == 0 else 1./dist 
             G.add_edge(node_labels[a], node_labels[b], weight=similarity)
-            # G.add_edge(a, b, weight=dist)
-    pos = nx.spring_layout(G)
+    if performer2watch:  # performer2watch used as an analysis flag, see above
+        print("Maximum distance:", maxd)
+    pos = nx.spring_layout(G, seed=seed)
     return G, pos, cols
 
 def show_Gpos(G, pos, cols):
-    # nx.draw_networkx_labels(G, pos)
     nx.draw(G, pos, node_color=cols, with_labels=True, edgelist=[])
     plt.show()
 
@@ -34,22 +40,3 @@ def show_one(i):
     G, pos, cols = compute_one(i)
     show_Gpos(G, pos, cols)
 
-def test32(c=1., d=2.):
-    H = nx.Graph()
-    H.add_node(0)
-    H.add_node(1)
-    H.add_node(2)
-    H.add_node(4)
-    H.add_node(5)
-    H.add_edge(0, 1, weight = c/1.)
-    H.add_edge(0, 2, weight = c/1.)
-    H.add_edge(1, 2, weight = c/1.)
-    H.add_edge(4, 5, weight = c/1.)
-    H.add_edge(0, 4, weight = c/d)
-    H.add_edge(0, 5, weight = c/d)
-    H.add_edge(1, 4, weight = c/d)
-    H.add_edge(1, 5, weight = c/d)
-    H.add_edge(2, 4, weight = c/d)
-    H.add_edge(2, 5, weight = c/d)
-    pos = nx.spring_layout(H)
-    return H, pos
