9e839c5b932265fe931d512e870d1e75d3c57ca4
Sean Carver <seancarverphd@gmail.com>
2020-05-06 18:44:31 -0400

    Now generates a list of primitives and their codes.

---
+++ malign/list_primitives.py
@@ -0,0 +1,21 @@
+import pickle
+
+with open("pipelines.pickle", "rb") as f:
+    pipelines = pickle.load(f)
+
+with open("translators.pickle", "rb") as f:
+    ta = pickle.load(f)
+    tb = pickle.load(f)
+
+plist = []
+for p in pipelines: 
+    for j in range(len(p['primitives'])): 
+        plist.append((p['primitives'][j], p['names'][j]))
+
+print(len(set(plist)))
+pset = set(plist)
+primitives = sorted([tb[p[0]]+': '+p[1] for p in pset])
+
+with open("primitive_list.txt", "w") as f:
+    for p in primitives:
+        f.write(p+"\n")
--- malign/pdf-command.sh
+++ malign/pdf-command.sh
@@ -1,1 +1,2 @@
 enscript --landscape -B -v -e@ outma0.txt -o - | ps2pdf - outma0.pdf
+enscript -B -v -e@ primitive_list.txt -o - | ps2pdf - primitive_list.pdf
