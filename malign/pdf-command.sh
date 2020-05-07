aa26a8603d21fa7916dbe100e7a8e496bcc866e8
Sean Carver <seancarverphd@gmail.com>
2020-05-07 18:24:57 -0400

    pdf-command now executes all other commands

--- malign/pdf-command.sh
+++ malign/pdf-command.sh
@@ -1,2 +1,5 @@
+./command.sh
+python parse_output.py
 enscript --landscape -B -v -e@ outma0.txt -o - | ps2pdf - outma0.pdf
+python list_primitive.py
 enscript -B -v -e@ primitive_list.txt -o - | ps2pdf - primitive_list.pdf
