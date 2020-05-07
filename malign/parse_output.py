d3b48334f5e137c4d265c0d619db8b44637c4d6d
Sean Carver <seancarverphd@gmail.com>
2020-05-07 18:28:15 -0400

    parse_output now creates file with 2 LETTER codes instead of hex

--- malign/parse_output.py
+++ malign/parse_output.py
@@ -1,3 +1,4 @@
+import copy
 import matplotlib
 import numpy as np
 import pandas as pd
@@ -12,22 +13,33 @@ with open("single_problem_data.pickle", "rb") as f:
 with open("outma0.hex", "r") as f:
     lines = f.readlines()
 
-k = 0
+prob = 0
+d = sp[prob]['translatorh2']
+d['--'] = '--'
 with open("outma0.txt", "w") as f:
-    n_non = 0
-    i = 0
+    n_nonpipeline = 0
+    i_pipeline_in_problem = 0
     for line in lines:
         if line[0:8] != 'pipeline':
-            print(line[0:8], 'NOT A PIPELINE:', line)
-            n_non += 1
+            n_nonpipeline += 1
             continue
         pipeline_num = int(line[9:13])
-        pipeline_col = show_graph.performer_colors[sp[k]['performers'][i]]
-        assert pipeline_num == sp[k]['pipeline_num'][i]
+        rest_of_line = line[13:]
+        line_pieces = rest_of_line.split(' ')
+        line_pieces_copy = copy.deepcopy(line_pieces)
+        i = len(line_pieces_copy)
+        for piece in line_pieces_copy[::-1]:
+            i -= 1  # step from end to front of list to avoid changing index when popping
+            if len(piece) != 2:
+                line_pieces.pop(i)
+        pipeline2 = '-'.join([d[piece] for piece in line_pieces])
+        print(pipeline2)
+        pipeline_col = show_graph.performer_colors[sp[prob]['performers'][i_pipeline_in_problem]]
+        assert pipeline_num == sp[prob]['pipeline_num'][i_pipeline_in_problem]
         f.write('@color{' +
                 str(matplotlib.colors.to_rgb(pipeline_col)[0]) + ' ' +
                 str(matplotlib.colors.to_rgb(pipeline_col)[1]) + ' ' +
                 str(matplotlib.colors.to_rgb(pipeline_col)[2]) + '}' +
-                '{:<10}'.format(sp[k]['performers'][i]) + line)
-        i += 1
+                '{:<10}'.format(sp[prob]['performers'][i_pipeline_in_problem]) + line[:14] + pipeline2 + '\n')
+        i_pipeline_in_problem += 1  # shouldn't increment if nonpipeline
 
