eadaf39158c35908cbf4ac738cdac636c4d8698a
Sean Carver <seancarverphd@gmail.com>
2020-05-22 20:00:15 -0400

    Analysis of pipeline measures complete.

--- edit_distance/edit_norm.py
+++ edit_distance/edit_norm.py
@@ -1,19 +1,22 @@
 import pickle
 import numpy as np
 
-with open("single_problem_data.pickle", "rb") as f:
-    sp = pickle.load(f)
-
-def lpnorm(prob, p, normalize=False):
+def lpnorm(prob=0, p=1, normalize=False, inp="single_problem_data.pickle"):
+    with open(inp, "rb") as f:
+        sp = pickle.load(f)
     df = sp[prob]['pairs']
-    if p == np.Inf and normalize:
-        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (max(dfr.distance)/len(dfr.distance))))
-    elif normalize:
-        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance**p))**(1/p)/len(dfr.distance)))
+    if p == 0 and normalize:
+        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance!=0)/len(dfr.distance))).sort_values(ascending=False))
+    elif p == np.Inf and normalize:
+        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (max(dfr.distance)/len(dfr.distance))).sort_values(ascending=False))
+    elif normalize and p!=0:
+        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance**p))**(1/p)/len(dfr.distance)).sort_values(ascending=False))
     elif p == np.Inf:
-        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (max(dfr.distance))))
+        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (max(dfr.distance))).sort_values(ascending=False))
+    elif p == 0:
+        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance!=0))).sort_values(ascending=False)) 
     else:
-        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance**p))**(1/p)))
+        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance**p))**(1/p)).sort_values(ascending=False))
 
 
 
