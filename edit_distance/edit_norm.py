86f4c41b53ad3a48b3997bca4a4a842b5451ab7a
Sean Carver <seancarverphd@gmail.com>
2020-05-31 03:42:04 -0400

    Added max_score, problem_name, and multimeasure to edit_norms.py

--- edit_distance/edit_norm.py
+++ edit_distance/edit_norm.py
@@ -1,5 +1,6 @@
-import pickle
 import numpy as np
+import pandas as pd
+import pickle
 
 def lpnorm(prob=0, p=1, normalize=False, inp="single_problem_data.pickle"):
     with open(inp, "rb") as f:
@@ -18,5 +19,26 @@ def lpnorm(prob=0, p=1, normalize=False, inp="single_problem_data.pickle"):
     else:
         return df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance**p))**(1/p)).sort_values(ascending=False)
 
-# def max_score(prob=0, 
+def max_score(prob=0):
+    with open("single_problem_data.pickle", "rb") as f:
+        sp = pickle.load(f)
+    df = pd.DataFrame({"performer": sp[prob]['performers'], "adjusted_score": sp[prob]['adjusted_scores']})
+    return df.groupby('performer').max().sort_values('adjusted_score', ascending=False)
 
+def multimeasure(prob=0):
+    common_index = set()
+    l1 = pd.DataFrame(lpnorm(prob=prob, p=1))
+    l1.columns = ['l1']
+    l2 = pd.DataFrame(lpnorm(prob=prob, p=2))
+    l2.columns = ['l2']
+    linf = pd.DataFrame(lpnorm(prob=prob, p=np.inf))
+    linf.columns = ['linf']
+    maxs = pd.DataFrame(max_score(prob=prob))
+    maxs.columns = ['max_score']
+    multi = pd.DataFrame(l1).join(pd.DataFrame(l2),how='inner').join(pd.DataFrame(linf),how='inner').join(pd.DataFrame(maxs),how='inner')
+    return multi
+
+def problem_name(prob=0):
+    with open("single_problem_data.pickle", "rb") as f:
+        sp = pickle.load(f)
+    return sp[prob]['type']
