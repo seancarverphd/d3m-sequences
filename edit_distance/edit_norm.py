9fbdf33dc2e15a1b18bf7e1abd62c3f7466ce0e9
Sean Carver <seancarverphd@gmail.com>
2020-05-31 00:19:12 -0400

    edit_norm.py now returns dataframe instead of just printing it

--- edit_distance/edit_norm.py
+++ edit_distance/edit_norm.py
@@ -6,17 +6,17 @@ def lpnorm(prob=0, p=1, normalize=False, inp="single_problem_data.pickle"):
         sp = pickle.load(f)
     df = sp[prob]['pairs']
     if p == 0 and normalize:
-        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance!=0)/len(dfr.distance))).sort_values(ascending=False))
+        return df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance!=0)/len(dfr.distance))).sort_values(ascending=False)
     elif p == np.Inf and normalize:
-        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (max(dfr.distance)/len(dfr.distance))).sort_values(ascending=False))
+        return df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (max(dfr.distance)/len(dfr.distance))).sort_values(ascending=False)
     elif normalize and p!=0:
-        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance**p))**(1/p)/len(dfr.distance)).sort_values(ascending=False))
+        return df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance**p))**(1/p)/len(dfr.distance)).sort_values(ascending=False)
     elif p == np.Inf:
-        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (max(dfr.distance))).sort_values(ascending=False))
+        return df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (max(dfr.distance))).sort_values(ascending=False)
     elif p == 0:
-        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance!=0))).sort_values(ascending=False)) 
+        return df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance!=0))).sort_values(ascending=False) 
     else:
-        print(df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance**p))**(1/p)).sort_values(ascending=False))
+        return df[df.performers0 == df.performers1].groupby('performers0').apply(lambda dfr: (sum(dfr.distance**p))**(1/p)).sort_values(ascending=False)
 
-
+# def max_score(prob=0, 
 
