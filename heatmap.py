7ad74722ce4466972185b5387c4f4db98a7e3561
jillcadams <jill.adams.18@cnu.edu>
2020-07-08 19:15:46 -0400

    annotated heatmap using pandas

--- heatmap.py
+++ heatmap.py
@@ -1,67 +1,46 @@
-import csv
+
 import pandas as pd
 import plotly.figure_factory as ff
-import plotly.graph_objects as go
 
 
 def get_colors(path):
-    with open(path) as csvfile:
-        df = pd.read_csv(csvfile)
+
+    with open(path) as small_df_csv:
+        df = pd.read_csv(small_df_csv)
+        y_values = df.performer.unique().tolist()
         df = df.pivot_table(index=['performer'],
                             columns=['category'], values='l2').fillna(0)
 
-        df.to_csv('grouped_data.csv')
-    return df
+        x_values = df.columns.values.tolist()
+        data = df.values.tolist()
+    return x_values, y_values, data
 
 
-def get_aster(path):
-    with open(path) as csvfile:
-        new_df = pd.read_csv(csvfile)
+def get_asterisk(path):
+
+    with open(path) as sumdf_csv:
+        new_df = pd.read_csv(sumdf_csv)
+        new_df.best_string = new_df.best_string.fillna(' ')
+
         new_df = new_df.pivot_table(index=['performer'],
                                     columns=['category'], values=['best_string'],
                                     aggfunc=lambda x: ''.join(str(v) for v in x)).fillna("")
 
-        new_df.to_csv('new_grouped_data.csv')
+        text = new_df.best_string.values.tolist()
+    return text
 
 
-def create_heatmap(df):
-        data = []
-        x_values = []
-        y_values = []
-        text = []
-        with open('grouped_data.csv') as csvlines:
-            reader = csv.reader(csvlines)
-            for line in reader:
-                if 'performer' in line:
-                    continue
-                else:
-                    y_values.append(line[0])
-                    item = line[1:]
-                    data.append(item)
-            for column in df.columns:
-                x_values.append(column)
+def create_heatmap(x_values, y_values, data, text):
 
-        with open('new_grouped_data.csv') as newlines:
-            reader = csv.reader(newlines)
-            for line in reader:
-                if 'best_string' in line:
-                    continue
-                if 'performer' in line:
-                    continue
-                if 'category' in line:
-                    continue
-                else:
-                    item = line[1:]
-                    text.append(item)
-
-        fig = ff.create_annotated_heatmap(z=data, x=x_values, y=y_values, annotation_text=text)
-        fig.update_xaxes(side="bottom")
-        fig.update_layout(xaxis=dict(title='Category'), yaxis=dict(title='Performer'))
-        fig.show()
-        fig.write_image("heatmap.pdf")
+    fig = ff.create_annotated_heatmap(z=data, x=x_values, y=y_values, annotation_text=text, showscale=True)
+    fig.update_xaxes(side="bottom")
+    fig.update_layout(xaxis=dict(title='Category'), yaxis=dict(title='Performer'))
+    fig.show()
+    fig.write_image("heatmap.pdf")
 
 
 if __name__ == '__main__':
-    get_colors('small_df.csv')
-    get_aster('sumdf.csv')
-    create_heatmap(get_colors('small_df.csv'))
+    x, y, data_values = get_colors('small_df.csv')
+    z_text = get_asterisk('sumdf.csv')
+    create_heatmap(x, y, data_values, z_text)
+s
