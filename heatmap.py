48f88d8b338d68d644267bdffe94165c1b2ede74
jillcadams <jill.adams.18@cnu.edu>
2020-07-08 14:32:07 -0400

    annotated heatmap

--- heatmap.py
+++ heatmap.py
@@ -1,21 +1,34 @@
-
 import csv
 import pandas as pd
-import plotly.express as px
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
 
 
-def create_heatmap(path):
-
+def get_colors(path):
     with open(path) as csvfile:
-        data = []
-        x_values = []
-        y_values = []
         df = pd.read_csv(csvfile)
         df = df.pivot_table(index=['performer'],
                             columns=['category'], values='l2').fillna(0)
 
         df.to_csv('grouped_data.csv')
+    return df
 
+
+def get_aster(path):
+    with open(path) as csvfile:
+        new_df = pd.read_csv(csvfile)
+        new_df = new_df.pivot_table(index=['performer'],
+                                    columns=['category'], values=['best_string'],
+                                    aggfunc=lambda x: ''.join(str(v) for v in x)).fillna("")
+
+        new_df.to_csv('new_grouped_data.csv')
+
+
+def create_heatmap(df):
+        data = []
+        x_values = []
+        y_values = []
+        text = []
         with open('grouped_data.csv') as csvlines:
             reader = csv.reader(csvlines)
             for line in reader:
@@ -28,15 +41,27 @@ def create_heatmap(path):
             for column in df.columns:
                 x_values.append(column)
 
-        fig = px.imshow(data,
-                        labels=dict(x="Category", y="Performer", color="l2 value"),
-                        x=x_values,
-                        y=y_values
-                       )
+        with open('new_grouped_data.csv') as newlines:
+            reader = csv.reader(newlines)
+            for line in reader:
+                if 'best_string' in line:
+                    continue
+                if 'performer' in line:
+                    continue
+                if 'category' in line:
+                    continue
+                else:
+                    item = line[1:]
+                    text.append(item)
+
+        fig = ff.create_annotated_heatmap(z=data, x=x_values, y=y_values, annotation_text=text)
         fig.update_xaxes(side="bottom")
+        fig.update_layout(xaxis=dict(title='Category'), yaxis=dict(title='Performer'))
         fig.show()
         fig.write_image("heatmap.pdf")
 
 
 if __name__ == '__main__':
-    create_heatmap('small_df.csv')
+    get_colors('small_df.csv')
+    get_aster('sumdf.csv')
+    create_heatmap(get_colors('small_df.csv'))
